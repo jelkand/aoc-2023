aabee0def704cf8b2e32fb9d89b76c4b8eeb15e3
Jack Anderson <jack@jackanderson.dev>
2023-12-04 08:39:08 -0600

    day 4 part 1

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,5 +1,45 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  def parse_line(line) do
+    [winners, numbers] =
+      line
+      |> String.split(": ", trim: true)
+      |> List.last()
+      |> String.split(" | ", trim: true)
+      |> Enum.map(&String.split(&1, " ", trim: true))
+      |> Enum.map(&Enum.map(&1, fn x -> String.to_integer(x) end))
+
+    number_set = MapSet.new(numbers)
+
+    if MapSet.size(number_set) != length(numbers),
+      do: IO.puts("Got set of numbers with duplicates")
+
+    {MapSet.new(winners), number_set}
+  end
+
+  def count_winners(lines) do
+    Enum.map(lines, fn {winners, numbers} ->
+      MapSet.intersection(winners, numbers) |> MapSet.size()
+    end)
+  end
+
+  def score(matches) do
+    matches
+    |> Enum.filter(fn count -> count > 0 end)
+    |> Enum.map(fn count -> 2 ** (count - 1) end)
+  end
+
+  def part1(args) do
+    args
+    |> parse_input()
+    |> count_winners()
+    |> score()
+    |> Enum.sum()
   end
 
   def part2(_args) do
