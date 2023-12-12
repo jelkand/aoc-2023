2266aa30fd7e9db943d567a47af6149e8e02e7c0
Jack Anderson <jack@jackanderson.dev>
2023-12-12 11:00:11 -0600

    d12p1

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -1,9 +1,16 @@
 defmodule AdventOfCode.Day12 do
   # 8832 too high
+  # 8464 too high
   def part1(args) do
-    args
-    |> parse_input()
-    |> solve_1()
+    input =
+      parse_input(args)
+
+    output = solve_1(input)
+
+    # filter out all of the ones that had trailing #'s
+    validate_output(input, output)
+    |> Enum.map(&length/1)
+    |> Enum.sum()
   end
 
   def part2(_args) do
@@ -26,8 +33,10 @@ defmodule AdventOfCode.Day12 do
   def solve_1(springs) do
     springs
     |> Enum.map(&solve_line/1)
-    |> List.flatten()
-    |> length()
+
+    # |> Enum.map(&length/1)
+    # |> Enum.sum()
+    # |> dbg
   end
 
   def solve_line({springs, broken_sections}) do
@@ -40,6 +49,7 @@ defmodule AdventOfCode.Day12 do
       |> Enum.join("")
       |> String.replace("S", ".")
       |> String.replace("P", "#")
+      |> String.replace("?", ".")
 
   def solve_line_internal({springs, [first_section_size | rest]}) do
     potential_placements =
@@ -50,8 +60,8 @@ defmodule AdventOfCode.Day12 do
         {{_, min_idx}, {_, max_idx}} = Enum.min_max_by(chunk, &elem(&1, 1))
         first_spring = Enum.find_index(springs, fn sym -> sym == "#" end)
 
-        Enum.all?(chunk, fn {sym, _} -> sym in ["?", "#"] end) and
-          (min_idx == 0 or Enum.at(springs, min_idx - 1, ".") not in ["#", "P"]) and
+        (min_idx == 0 or Enum.at(springs, min_idx - 1, ".") not in ["#", "P"]) and
+          Enum.all?(chunk, fn {sym, _} -> sym in ["?", "#"] end) and
           Enum.at(springs, max_idx + 1, ".") not in ["#", "P"] and
           min_idx <= first_spring
       end)
@@ -79,5 +89,17 @@ defmodule AdventOfCode.Day12 do
     end)
     |> List.flatten()
     |> Enum.uniq()
+  end
+
+  def validate_output(input, output) do
+    chunks = input |> Enum.map(&elem(&1, 1))
+
+    Enum.zip(chunks, output)
+    |> Enum.map(fn {chunked, outputs} ->
+      Enum.map(outputs, fn str ->
+        String.split(str, ".", trim: true) |> Enum.map(&String.length/1)
+      end)
+      |> Enum.filter(fn translated -> translated == chunked end)
+    end)
   end
 end
